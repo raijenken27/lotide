74bc4a05b9cdc152ea2c3ce494c3dcc0ceb89c10
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-08-31 10:38:43 -0600

    Implement middle

---
+++ middle.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+function assertArraysEqual(actual, expected) {
+    
+  }
+  
+  function eqArrays(array1, array2) {
+   
+  }
+  
+  function middle(array) {
+    const length = array.length;
+    const middleIndex = Math.floor(length / 2);
+  
+    if (length <= 2) {
+      return [];
+    }
+  
+    if (length % 2 === 0) {
+      return [array[middleIndex - 1], array[middleIndex]];
+    } else {
+      return [array[middleIndex]];
+    }
+  }
+  
+  assertArraysEqual(middle([1]), []);
+  assertArraysEqual(middle([1, 2]), []);
+  assertArraysEqual(middle([1, 2, 3]), [2]);
+  assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+  assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+  assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  
+  console.log("Test assertions completed!");
+  
