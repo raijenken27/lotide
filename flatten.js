9a785b9a884a079097d98f100abed21d82bc005b
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-08-31 10:34:28 -0600

    Implement flatten

---
+++ flatten.js
@@ -0,0 +1,30 @@
+function assertArraysEqual(actual, expected) {
+  
+  }
+  
+  function eqArrays(array1, array2) {
+   
+  }
+  
+  function flatten(arr) {
+    const flattenedArray = [];
+  
+    for (const element of arr) {
+      if (Array.isArray(element)) {
+        for (const nestedElement of element) {
+          flattenedArray.push(nestedElement);
+        }
+      } else {
+        flattenedArray.push(element);
+      }
+    }
+  
+    return flattenedArray;
+  }
+  
+
+  assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); 
+  
+
+  console.log("Test cases completed!"); 
+  
