0c7c0da7219c4e88b489aef12a85288b697e3bd2
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-02 13:31:51 -0600

    implement findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+// findKeyByValue.js
+
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+  
+  const findKeyByValue = function(object, value) {
+    for (const key in object) {
+      if (object[key] === value) {
+        return key;
+      }
+    }
+    return undefined;
+  };
+  
+  module.exports = findKeyByValue;
+  
+  // Test cases
+  const bestTVShowsByGenre = { 
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+  
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+  assertEqual(findKeyByValue({}, "The Wire"), undefined);
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "Friends"), undefined);
+  
