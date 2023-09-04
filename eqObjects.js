c8d9b9405a1950fb724458d5b090fdee920883d0
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-04 15:14:17 -0600

    Implement eqObjects

---
+++ eqObjects.js
@@ -0,0 +1,58 @@
+// eqObjects.js
+
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+  
+  const eqArrays = function(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+  
+  const eqObjects = function(object1, object2) {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+  
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+  
+    for (const key of keys1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  
+    return true;
+  };
+  
+  // Test cases
+  const shirtObject = { color: "red", size: "medium" };
+  const anotherShirtObject= { size: "medium", color: "red" };
+  assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
+  
+  const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+  assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
+  
+  const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+  const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+  assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject), true);
+  
+  const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+  assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false);
+  
