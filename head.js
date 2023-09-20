86c04d024a1fa65842a859718ad74bcda88c570f
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-20 16:30:32 -0600

    Incorporating Mocha & Chai

--- head.js
+++ head.js
@@ -12,10 +12,29 @@
     return array[0]; // Return the first element of the array
   }; */
   
-  const assertEqual = require('./assertEqual');
+ // const assertEqual = require('./assertEqual');//
 
  /*  assertEqual(head([5, 6, 7]), 5);
   assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
   assertEqual(head([]), undefined); // An empty array should yield undefined as its head
   assertEqual(head([42]), 42); // An array with only one element should still yield that one element as its head
 */
+
+// head.js
+
+const assertEqual = require('./assertEqual');
+
+const head = function(arr) {
+  if (arr && arr.length > 0) {
+    return arr[0];
+  }
+  return undefined; // Return undefined if the array is empty or undefined
+};
+
+module.exports = head;
+
+// Test cases for head function
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(['hello', 'world']), 'hello');
+assertEqual(head([]), undefined);
+assertEqual(head(undefined), undefined);
