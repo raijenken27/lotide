b527212226cee3a1f3d8b2242081076ae2365b6b
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-21 14:47:58 -0600

    Recursive eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -35,9 +35,36 @@
 
   // eqArrays.js
 
-const eqArrays = function(array1, array2) {
+/* const eqArrays = function(array1, array2) {
     // Implementation of eqArrays function
   };
   
   module.exports = eqArrays; // Export the eqArrays function
+  
+  */
+
+  const eqArrays = function(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+    
+    for (let i = 0; i < arr1.length; i++) {
+      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+        // If both elements are arrays, recursively compare them
+        if (!eqArrays(arr1[i], arr2[i])) {
+          return false;
+        }
+      } else if (arr1[i] !== arr2[i]) {
+        // If elements are not arrays, compare them directly
+        return false;
+      }
+    }
+    
+    return true;
+  };
+  
+  // Test cases to demonstrate that eqArrays works for nested arrays
+  console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // Should return true
+  console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // Should return false
+  console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // Should return false
   
