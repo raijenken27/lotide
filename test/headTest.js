f9a03b3bc69c90161d186e296195f13b2c79e255
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-22 15:41:38 -0600

    Incorporating Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,19 +1,15 @@
-const head = require('../head');
-
-
 // test/headTest.js
+// const head = require('../head');
 
-/* const assert = require('chai').assert;
+const assert = require('chai').assert;
 const head   = require('../head');
 
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
-});
 
-it("returns '5' for ['5']", () => {
+  it("returns '5' for ['5']", () => {
     assert.strictEqual(head(['5']), '5'); 
   });
-
-  */
+});
