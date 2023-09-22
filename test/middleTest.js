5ccff213deb9f8c73194221a5896523ed22c8437
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-22 12:04:00 -0600

    Refactoring middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,20 +1,2 @@
-/* test/middleTest.js
-
-const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-
-// TEST CODE for middle function
-// ...
-
-*/
-
-const assert = require('chai').assert;
-const middle = require('../middle');
-
-describe("#middle", () => {
-  it("returns [2] for [1, 2, 3]", () => {
-    assert.deepEqual(middle([1, 2, 3]), [2]);
-  });
-
-  // Add more test cases as needed
-});
+const middle = require('../middle'); 
+const assertArraysEqual = require('../assertArraysEqual'); 
