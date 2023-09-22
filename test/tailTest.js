3d30ad671ef107e64ebdba53985752e0d7744ccd
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-22 11:57:15 -0600

    Resubmition of Refactorting tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,21 +1,4 @@
-/* test/tailTest.js
+// test/tailTest.js
 
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
-
-// TEST CODE for tail function
-// ...
-
-
-*/
-
-const assert = require('chai').assert;
-const tail = require('../tail');
-
-describe("#tail", () => {
-  it("returns [2, 3] for [1, 2, 3]", () => {
-    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
-  });
-
-  // Add more test cases as needed
-});
