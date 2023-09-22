7562a7d9ecadbd67cadbdcd299ceea11e64298aa
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-22 15:52:58 -0600

    Exporting All Functions II

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-/* const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
 
-module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
-};
 
-*/
-
-// Implement Object Shorthand
+// Exporting All Functions II
 
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
-// Import other functions if you have them
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
   head,
   tail,
   middle,
-  // Add more functions here using the shorthand syntax
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
 };
