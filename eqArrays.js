d62c93291aa71da97055d001d731f6c418930995
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-28 14:37:53 -0600

    Refectoring eqArrays but accidentally wrote on eqObjects.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,25 +1,15 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
-  }
-}
-
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
     return false
   }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
       return false
     }
   }
+
   return true
 }
 
-// Test cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true)
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false)
+module.exports = eqArrays
