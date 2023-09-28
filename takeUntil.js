3f0b8078f3d81fc91c42e9c983db52e0c21bee29
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-28 13:17:38 -0600

    Cleaning takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,20 +1,40 @@
-const takeUntil = function(array, callback) {
-    const results = [];
-    for (let item of array) {
-      if (callback(item)) {
-        break;
-      }
-      results.push(item);
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log('✅ Assertion Passed:', actual, '==', expected)
+  } else {
+    console.log('🛑 Assertion Failed:', actual, '!=', expected)
+  }
+}
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
     }
-    return results;
-  };
-  
-  // Test cases
-  const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-  const results1 = takeUntil(data1, x => x < 0);
-  console.log(results1); // Output: [1, 2, 5, 7, 2]
-  
-  const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-  const results2 = takeUntil(data2, x => x === ',');
-  console.log(results2); // Output: [ 'I've', 'been', 'to', 'Hollywood' ]
-  
+  }
+  return true
+}
+
+const takeUntil = function (array, callback) {
+  const result = []
+  for (const item of array) {
+    if (!callback(item)) {
+      result.push(item)
+    } else {
+      break
+    }
+  }
+  return result
+}
+
+// Test cases
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5]
+const results1 = takeUntil(data1, x => x < 0)
+assertArraysEqual(results1, [1, 2, 5, 7, 2])
+
+const data2 = ['I\'ve', 'been', 'to', 'Hollywood', ',', 'I\'ve', 'been', 'to', 'Redwood']
+const results2 = takeUntil(data2, x => x === ',')
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood'])
