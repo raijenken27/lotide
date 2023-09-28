68c72b72e2e9f0c74ce88bfdc2ab05bc35b0b81b
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-28 10:49:40 -0600

    Eslinting the code for head.js

--- head.js
+++ head.js
@@ -1,26 +1,19 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
-  
-  const head = function(array) {
-    if (array.length === 0) {
-      return undefined;
-    }
-    return array[0];
-  };
-  
-  // Test cases
-  assertEqual(head([5, 6, 7]), 5);
-  assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-  assertEqual(head([]), undefined);
-  assertEqual(head([42]), 42);
-  
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
 
+const head = function (arr) {
+  if (arr.length === 0) {
+    return undefined
+  }
+  return arr[0]
+}
 
-  console.log("Test assertions completed!");  
-
-  module.exports = head;
+assertEqual(head([5, 6, 7]), 5)
+assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello')
+assertEqual(head([1]), 1)
+assertEqual(head([]), undefined)
