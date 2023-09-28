949e2f47901500948ceec3f3cc065d1893af819f
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-28 12:21:02 -0600

    Cleaning findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,26 @@
- const findKeyByValue = function(object, value) {
-    for (const key in object) {
-      if (object[key] === value) {
-        return key;
-      }
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
+
+const findKeyByValue = function (object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key
     }
-    return undefined;
-  };
-  
-  module.exports = findKeyByValue;
-  
+  }
+  return undefined
+}
+
+// Test cases
+const bestTVShowsByGenre = {
+  sci_fi: 'The Expanse',
+  comedy: 'Brooklyn Nine-Nine',
+  drama: 'The Wire'
+}
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Wire'), 'drama')
+assertEqual(findKeyByValue(bestTVShowsByGenre, 'That \'70s Show'), undefined)
