68ad1cd65bb5486bce9db94e6866f7cdd5b53e50
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-28 11:18:42 -0600

    Cleaning assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,18 @@
-// Import the eqArrays function
-const eqArrays = require('./eqArrays')
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`)
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`)
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
   }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true
 }
 
-module.exports = assertArraysEqual
+const assertArraysEqual = function (arr1, arr2) {
+  const message = eqArrays(arr1, arr2)
+    ? '✅✅✅ Assertion Passed: Arrays are equal'
+    : '🛑🛑🛑 Assertion Failed: Arrays are not equal'
+  console.log(message)
+}
