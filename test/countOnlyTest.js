ce3180be0cd04506c4931f0a0a162fee3873f127
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-28 15:26:10 -0600

    Refactoring countOnly.js

--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -1,11 +1,33 @@
-const assert = require('chai').assert;
-const countOnly = require('../countOnly');
+const assertEqual = require('../assertEqual')
+const countOnly = require('../countOnly')
 
-describe('#countOnly', () => {
-  it('should return { "a": 2, "b": 1 } when given ["a", "b", "a"], { "a": true, "b": false }', () => {
-    const inputArray = ["a", "b", "a"];
-    const itemsToCount = { "a": true, "b": false };
-    const expected = { "a": 2, "b": 1 };
-    assert.deepEqual(countOnly(inputArray, itemsToCount), expected);
-  });
-});
+// Test case 1
+const firstNames = [
+  'Karl',
+  'Salima',
+  'Agouhanna',
+  'Fang',
+  'Kavith',
+  'Jason',
+  'Salima',
+  'Fang',
+  'Joe'
+]
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false
+})
+
+assertEqual(result1.Jason, 1)
+assertEqual(result1.Karima, undefined)
+assertEqual(result1.Fang, 2)
+assertEqual(result1.Agouhanna, undefined)
+
+// Test case
+const result2 = countOnly(['a', 'b', 'a'], { a: true, b: false })
+
+assertEqual(result2.a, 2)
+assertEqual(result2.b, undefined)
