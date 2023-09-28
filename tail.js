7b7690e43ac2c9d76e9282c7559253144d95ce53
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-28 11:04:19 -0600

    Clean tail.js and added eslint into gitignore

--- tail.js
+++ tail.js
@@ -1,9 +1,23 @@
-const tail = function(array) {
-    if (array.length <= 1) {
-      return [];
-    }
-    return array.slice(1);
-  };
-  
-  module.exports = tail;
-  
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
+
+const tail = function (arr) {
+  if (arr.length <= 1) {
+    return []
+  }
+  return arr.slice(1)
+}
+
+// Test cases
+const words = ['Yo Yo', 'Lighthouse', 'Labs']
+tail(words)
+assertEqual(words.length, 3)
+
+assertEqual(tail([1, 2, 3]).toString(), [2, 3].toString())
+assertEqual(tail(['Hello']).toString(), [].toString())
+assertEqual(tail([]).toString(), [].toString())
