b061ffd1a9f10bb17401e1f4468d1125ba84983a
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-28 12:16:14 -0600

    Cleaning letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,19 +1,36 @@
- const letterPositions = function(sentence) {
-    const results = {};
-  
-    for (let i = 0; i < sentence.length; i++) {
-      const character = sentence[i].toLowerCase();
-  
-      if (character !== ' ') {
-        if (!results[character]) {
-          results[character] = [];
-        }
-        results[character].push(i);
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) return false
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false
+  }
+  return true
+}
+
+const letterPositions = function (sentence) {
+  const results = {}
+  for (let i = 0; i < sentence.length; i++) {
+    const char = sentence[i]
+    if (char !== ' ') {
+      if (!results[char]) {
+        results[char] = [i]
+      } else {
+        results[char].push(i)
       }
     }
-  
-    return results;
-  };
-  
-  module.exports = letterPositions;
-  
+  }
+  return results
+}
+
+const result = letterPositions('hello')
+assertArraysEqual(result.h, [0])
+assertArraysEqual(result.e, [1])
+assertArraysEqual(result.l, [2, 3])
+assertArraysEqual(result.o, [4])
