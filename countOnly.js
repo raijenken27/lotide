f86a30c8407ff63b056eb35fec98ea392a94ac84
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-28 11:57:10 -0600

    Cleaning countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -1,46 +1,46 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
 
- const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
-  
-  const countOnly = function(allItems, itemsToCount) {
-    const results = {};
-  
-    for (const item of allItems) {
-      if (itemsToCount[item]) {
-        if (results[item]) {
-          results[item] += 1;
-        } else {
-          results[item] = 1;
-        }
+const countOnly = function (allItems, itemsToCount) {
+  const result = {}
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (result[item]) {
+        result[item] += 1
+      } else {
+        result[item] = 1
       }
     }
-  
-    return results;
-  };
+  }
+  return result
+}
 
-  // Test cases
-  const firstNames = [
-    "Karl",
-    "Salima",
-    "Agouhanna",
-    "Fang",
-    "Kavith",
-    "Jason",
-    "Salima",
-    "Fang",
-    "Joe"
-  ];
-  
-  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-  
-  assertEqual(result1["Jason"], 1);
-  assertEqual(result1["Karima"], undefined);
-  assertEqual(result1["Fang"], 2);
-  assertEqual(result1["Agouhanna"], undefined);
-  
-  console.log("Test cases completed!");
+// Test case
+const firstNames = [
+  'Karl',
+  'Salima',
+  'Agouhanna',
+  'Fang',
+  'Kavith',
+  'Jason',
+  'Salima',
+  'Fang',
+  'Joe'
+]
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false
+})
+
+assertEqual(result1.Jason, 1)
+assertEqual(result1.Karima, undefined)
+assertEqual(result1.Fang, 2)
+assertEqual(result1.Agouhanna, undefined)
