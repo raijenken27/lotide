c727535ac370ca20ea11f8765a6e21d718370dce
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-28 12:10:01 -0600

    Cleaning countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -1,31 +1,30 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
 
+const countLetters = function (sentence) {
+  const result = {}
 
-  const countLetters = function(sentence) {
-    const letterCount = {};
-  
-    for (const letter of sentence) {
-      if (letter !== ' ') { // Exclude spaces from counting
-        if (letterCount[letter]) {
-          letterCount[letter] += 1;
-        } else {
-          letterCount[letter] = 1;
-        }
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+      if (result[letter]) {
+        result[letter] += 1
+      } else {
+        result[letter] = 1
       }
     }
-  
-    return letterCount;
-  };
-  
-  // Test cases
-  const result = countLetters("LHL");
-  assertEqual(result["L"], 2);
-  assertEqual(result["H"], 1);
-  
-  console.log("Test cases completed!");
+  }
+
+  return result
+}
+
+// Test cases
+const result = countLetters('LHL')
+assertEqual(result.L, 2)
+assertEqual(result.H, 1)
+
+console.log('Test cases completed!')
