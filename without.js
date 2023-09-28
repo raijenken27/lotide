5ed8c0d15f3d13859f94dac2ceaa4716b1b1cb0c
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-28 11:22:48 -0600

    Clean without.js

--- without.js
+++ without.js
@@ -1,31 +1,41 @@
-function assertArraysEqual(actual, expected) {
- 
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
   }
-  
-  function eqArrays(array1, array2) {
-
-  }
-
-  function without(source, itemsToRemove) {
-    let filteredArray = [];
-  
-    for (const item of source) {
-      if (!itemsToRemove.includes(item)) {
-        filteredArray.push(item);
-      }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
     }
-  
-    return filteredArray;
   }
-  
+  return true
+}
 
-  assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); 
-  assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); 
-  
-  
-  const words = ["hello", "world", "lighthouse"];
-  without(words, ["lighthouse"]);
-  assertArraysEqual(words, ["hello", "world", "lighthouse"]); 
-  
-  
-  console.log("Test cases completed!");
+const assertArraysEqual = function (arr1, arr2) {
+  const message = eqArrays(arr1, arr2)
+    ? '✅✅✅ Assertion Passed: Arrays are equal'
+    : '🛑🛑🛑 Assertion Failed: Arrays are not equal'
+  console.log(message)
+}
+
+const without = function (source, itemsToRemove) {
+  const result = []
+  for (const item of source) {
+    if (!itemsToRemove.includes(item)) {
+      result.push(item)
+    }
+  }
+  return result
+}
+
+// Test cases
+const words = ['hello', 'world', 'lighthouse']
+const result1 = without(words, ['lighthouse'])
+assertArraysEqual(result1, ['hello', 'world'])
+
+const numbers = [1, 2, 3, 4, 5]
+const result2 = without(numbers, [2, 4])
+assertArraysEqual(result2, [1, 3, 5])
+
+const emptyArray = []
+const result3 = without(emptyArray, [])
+assertArraysEqual(result3, [])
