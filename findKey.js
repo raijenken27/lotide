3d522ac5466d07d6453c7e2495d7d8fe2255d546
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-29 17:05:20 -0600

    Refactoring findKey and map, but forget to refactoring 1 by 1

--- findKey.js
+++ findKey.js
@@ -1,37 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKey = function(object, callback) {
-  for (const key in object) {
-    if (callback(object[key])) {
-      return key;
+  for (let item in object) {
+    if (callback(object[item])) {
+      return item;
     }
   }
-  return undefined;
 };
 
-// Test cases
-const restaurantRatings = {
-  'Blue Hill': { stars: 1 },
-  Akaleri: { stars: 3 },
-  noma: { stars: 2 },
-  elBulli: { stars: 3 },
-  Ora: { stars: 2 },
-  Akelarre: { stars: 3 }
-};
-
-const result1 = findKey(restaurantRatings, x => x.stars === 2);
-assertEqual(result1, 'noma');
-const result2 = findKey(restaurantRatings, x => x.stars === 4);
-assertEqual(result2, undefined);
-
-const result3 = findKey(restaurantRatings, x => x.stars === 1);
-assertEqual(result3, 'Blue Hill');
-
-const result4 = findKey(restaurantRatings, x => x.stars === 3);
-assertEqual(result4, 'Akaleri');
+module.exports = findKey;
--- map.js
+++ map.js
@@ -1,39 +1,9 @@
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log('✅ Assertion Passed:', actual, '==', expected);
-  } else {
-    console.log('🛑 Assertion Failed:', actual, '!=', expected);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const map = function(array, callback) {
-  const results = [];
-  for (const item of array) {
+  let results = [];
+  for (let item of array) {
     results.push(callback(item));
   }
   return results;
 };
 
-// Test cases
-const words = ['ground', 'control', 'to', 'major', 'tom'];
-
-const upperCaseWords = map(words, word => word.toUpperCase());
-assertArraysEqual(upperCaseWords, ['GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM']);
-
-const wordLengths = map(words, word => word.length);
-assertArraysEqual(wordLengths, [6, 7, 2, 5, 3]);
-
-const exclamations = map(words, word => word + '!');
-assertArraysEqual(exclamations, ['ground!', 'control!', 'to!', 'major!', 'tom!']);
+module.exports = map;
