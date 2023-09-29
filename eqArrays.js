5290fc6d4ecc58e840550ea7b6b77ef6e41b4b5a
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-29 16:02:59 -0600

    Refactoring and cleaning eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,26 @@
 const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
 
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
+  let equal = true;
+  // Check if the array1 and array2 have the same length.
+  if (array1.length === array2.length) {
+    // Loop through the elements of array1 and array2 for comparison.
+    for (let i = 0; i < array1.length; i++) {
+      if (Array.isArray(array1[i])) {
+        equal =  eqArrays(array1[i], array2[i]);
+      } else if (array1[i] === array2[i]) {
+        // // If elements are of the same value and type, mark as equal. Otherwise, mark as not equal and exit the loop.
+        equal = true;
+      } else {
+        equal = false;
+        return equal;
+      }
     }
+  } else {
+    equal = false;
+    return equal;
   }
 
-  return true;
+  return equal;
 };
 
 module.exports = eqArrays;
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,49 @@
-const assertEqual = require('../assertEqual');
-const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
+const index = require('../index');
+const assertEqual = index.assertEqual;
+const eqArrays = index.eqArrays;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays([1], []), false);
+//TEST CODE
+describe("#eqArrays", () => {
+  it("returns true", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it("returns false", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+
+  it("returns true", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+
+  it("returns false", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+
+  it("returns true", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4,[5, 6]]], [[2, 3], [4, [5,6]]]), true);
+  });
+
+  it("returns false", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+  });
+
+});
+
+//TEST CODE
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // false
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // true
+
+console.log(eqArrays([[2, 3], [4,[5, 6]]], [[2, 3], [4, [5,6]]])); // => true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // false
+
+module.exports = eqArrays;
