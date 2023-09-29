a423120bdc441abb4b0b3933c7c6b7a98b044039
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-29 16:54:57 -0600

    Refactoring eqObjects + test

--- eqObjects.js
+++ eqObjects.js
@@ -1,36 +1,25 @@
-const eqArrays = require('./eqArrays');
+const eqArrays = require("./eqArrays");
 
 const eqObjects = function(object1, object2) {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-
-  if (keys1.length !== keys2.length) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
-
-  for (const key of keys1) {
+  for (let key in object1) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
-        return false;
-      }
-    } else if (object1[key] !== object2[key]) {
+      return eqArrays(object1[key], object2[key]);
+      
+    } else if ((typeof(object1[key]) !== typeof({}) && typeof(object2[key]) !== typeof({})) && (object1[key] !== object2[key])) {
       return false;
+
+    } else if ((typeof(object1[key]) === typeof({}) || typeof(object1[key]) === typeof({})) && eqObjects(object1[key], object2[key]) === false) {
+      return false;
+
+      // Check if the values associated with the current key are objects, and recursively compare them.
+    } else if (typeof(object1[key]) === typeof({}) || typeof(object1[key]) === typeof({})) {
+      eqObjects(object1[key], object2[key]);
     }
   }
-
   return true;
 };
 
-const shirtObject = { color: 'red', size: 'medium' };
-const anotherShirtObject = { size: 'medium', color: 'red' };
-console.log(eqObjects(shirtObject, anotherShirtObject)); // => true
-
-const longSleeveShirtObject = { size: 'medium', color: 'red', sleeveLength: 'long' };
-console.log(eqObjects(shirtObject, longSleeveShirtObject)); // => false
-
-const multiColorShirtObject = { colors: ['red', 'blue'], size: 'medium' };
-const anotherMultiColorShirtObject = { size: 'medium', colors: ['red', 'blue'] };
-console.log(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject));
-
-const longSleeveMultiColorShirtObject = { size: 'medium', colors: ['red', 'blue'], sleeveLength: 'long' };
-console.log(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject));
+module.exports = eqObjects;
