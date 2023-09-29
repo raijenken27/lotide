3f658a6104e6ed53da69ec374f549a5bf7fe37f6
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-09-29 14:27:33 -0600

    Refactoring and cleaning head + incorporating chai

--- head.js
+++ head.js
@@ -1,7 +1,4 @@
 const head = function(array) {
-  if (array.length === 0) {
-    return undefined;
-  }
   return array[0];
 };
 
--- test/headTest.js
+++ test/headTest.js
@@ -1,22 +1,31 @@
-const assert = require('chai').assert;
-const head = require('../head');
-const describe = require('mocha').describe;
-const it = require('mocha').it;
+// Import the 'assert' function from the 'chai' library.
+const { assert } = require("chai");
 
-describe('#head', () => {
-  it('returns 5 for [5, 6, 7]', () => {
+// Import the 'head' function from the 'head' module.
+const head = require("../head");
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
     assert.strictEqual(head([5, 6, 7]), 5);
   });
 
-  it('returns \'Hello\' for [\'Hello\', \'Lighthouse\', \'Labs\']', () => {
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
     assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
   });
 
-  it('returns 1 for [1]', () => {
+  it("returns 1 for [1]", () => {
     assert.strictEqual(head([1]), 1);
   });
 
-  it('returns undefined for an empty array', () => {
-    assert.isUndefined(head([]));
+  it("returns undefined for an empty array []", () => {
+    assert.strictEqual(head([]), undefined);
   });
 });
